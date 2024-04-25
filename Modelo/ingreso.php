f541f5f1df8c3d1143507bdb33136d7bf962b81f
PatriciaM.Silva <pmsilvaacuna@gmail.com>
2024-04-25 12:07:03 -0300

    modificacion sentencia sql para el loguin

--- Modelo/ingreso.php
+++ Modelo/ingreso.php
@@ -2,8 +2,8 @@
 $conexion=mysqli_connect("localhost","root","","cooperadora") or
     die("Problemas con la conexión");
 
-$registros=mysqli_query($conexion,"select *,id_usuario,
-                        Usuario, Contrasena, Id_tipodeusuario from usuario where Usuario ='$_REQUEST[usuario]' and Contrasena = '$_REQUEST[contrasena]'") or
+$registros=mysqli_query($conexion,"select id_usuario,
+                        Usuario,contrasenia,Id_tipodeusuario,estado from usuario where Usuario ='$_REQUEST[usuario]' and contrasenia = '$_REQUEST[contrasena]'") or
   die ("problemas en el select:".mysqli_error($conexion));
 session_start();
 if ($reg=mysqli_fetch_array($registros))
@@ -12,7 +12,7 @@ if ($reg=mysqli_fetch_array($registros))
     if ($reg ['estado'] == 1){
          $_SESSION['usuario'] = $reg['usuario'];
          $_SESSION['Id_tipodeusuario'] = $reg ['Id_tipodeusuario'];
-          $_SESSION['id_usuario'] = $reg ['id_usurio']; // Almaceno id_usuario
+          $_SESSION['id_usuario'] = $reg ['id_usuario']; // Almaceno id_usuario
             if ($reg['Id_tipodeusuario']==1){
                 $_SESSION['usuario'];
                     header('location: ..\Vista\accesoAceptadoAdmin.html');
