f50664f9999a9e59254a542c9e2d233ac3e59b5a
PatriciaM.Silva <pmsilvaacuna@gmail.com>
2024-05-04 00:31:08 -0300

    mejora loguin y vista pagina de error

--- Modelo/ingreso.php
+++ Modelo/ingreso.php
@@ -5,23 +5,37 @@ $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 $conexion = conexion();
 
-$query = "SELECT * FROM usuario WHERE Usuario = '$usuario' AND contrasenia = '$contrasena' AND Usuario_activo = 1 AND Id_tipoUsuario = 1";
+$query = "SELECT * FROM usuario WHERE Usuario = '$usuario' AND contrasenia = '$contrasena' AND Usuario_activo = 1";
 $result = $conexion->query($query);
 
 if ($result->num_rows > 0) {
+    $usuarioData = $result->fetch_assoc();
+    $idUsuario = $usuarioData['Id_usuario'];
+    
     // Iniciar una nueva sesión o reanudar la existente
     session_start();
 
     // Agregar variables a la sesión
     $_SESSION['usuario'] = $usuario;
-    $_SESSION['id_Usuario'] = $result->fetch_assoc()['Id_usuario'];
+    $_SESSION['id_Usuario'] = $idUsuario;
 
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Pragma: no-cache");
 
     // Redirigir al usuario a la página correspondiente
-    header("Location: ../Vista/accesoAceptadoAdmin.html");
+    if ($idUsuario == 1) {
+        header("Location: ../Vista/accesoAceptadoAdmin.html");
+    } elseif ($idUsuario == 2) {
+        header("Location: ../Vista/accesoAceptadoOperador.html");
+    } else {
+        // Redirigir a una página de error o manejar otro caso
+        // Por ejemplo:
+         header("Location: ../Vista/error.html");
+    }
 } else {
-    header("Location: ../Vista/accesoAceptadoOperador.html");
+    // Redirigir a una página de error o manejar otro caso
+    // Por ejemplo:
+    header("Location: ../Vista/error.html");
 }
+
 ?>
