3351b9bcea21662cabb879eea76932be96d89f43
sol <132224229+Danielacar94@users.noreply.github.com>
2024-06-14 19:49:40 -0300

    Create buscar.js
    para traer el dni del usuario

---
+++ Controlador/buscar.js
@@ -0,0 +1,36 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buscarForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir la recarga de la página
+
+        let Dni_usuario = document.getElementById('Dni_usuario').value;
+
+        fetch('../Modelo/buscar_usuario.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'Dni_usuario=' + encodeURIComponent(Dni_usuario)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.estado === 'OK') {
+                // Redirigir a actualizarUsuario.html con los datos del DNI
+                window.location.href = `../Vista/actualizarUsuario.html?Dni_usuario=${Dni_usuario}&usuario=${encodeURIComponent(data.Usuario)}&contrasenia=${encodeURIComponent(data.Contrasenia)}`;
+            } else {
+                document.getElementById('mensaje').textContent = data.msg || 'DNI no encontrado.';
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    });
+    // Manejar el botón de retorno
+    const returnButton = document.getElementById('returnButton');
+    if (returnButton) {
+        returnButton.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevenir cualquier acción predeterminada
+
+            window.location.href = '../Vista/configuracionUsuario.html'; // Redirigir a gestionUsuario.html
+        });
+    }
+});
