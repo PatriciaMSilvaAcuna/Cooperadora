5db655001f2de08938875f5cd7ba20ec654c71e9
Fernando Guiñazú <81384027+Fergui00@users.noreply.github.com>
2024-09-19 20:54:29 -0300

    Add files via upload
    probar si asi anda el pdf del pago

--- Modelo/setPago.php
+++ Modelo/setPago.php
@@ -1,6 +1,5 @@
 <?php
 session_start(); 
-
 include_once('conexion.php'); 
 
 $mysqli = conexion();
@@ -10,13 +9,13 @@ if (!isset($_SESSION['idusuario'])) {
     exit();
 }
 
+// Captura de datos
 $valorAbonado = $_POST['valorAbonado'];
 $fecha = $_POST['fecha'];
 $idmetodopago = $_POST['metodoPago'];
 $idconcepto = $_POST['concepto'];
-$idalumno = $_POST['idalumno']; // ID del alumno obtenido
-
-$idusuario = $_SESSION['idusuario']; // Obtener el ID del usuario desde la sesión
+$idalumno = $_POST['idalumno']; 
+$idusuario = $_SESSION['idusuario']; 
 
 $query = "INSERT INTO cargapago (valorabonado, fecha, idmetodopago, idconcepto, idalumno, idusuario) VALUES (?, ?, ?, ?, ?, ?)";
 $stmt = $mysqli->prepare($query);
@@ -24,7 +23,8 @@ $stmt = $mysqli->prepare($query);
 if ($stmt) {
     $stmt->bind_param('ssiiii', $valorAbonado, $fecha, $idmetodopago, $idconcepto, $idalumno, $idusuario);
     if ($stmt->execute()) {
-        echo json_encode(['status' => 'success', 'message' => 'Pago registrado con éxito']);
+        $idcargapago = $stmt->insert_id; // Captura el ID del pago
+        echo json_encode(['status' => 'success', 'message' => 'Pago registrado con éxito', 'idcargapago' => $idcargapago]);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Error al registrar el pago']);
     }
